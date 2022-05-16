58438bad39e5ac84d53109d72f2f5b6146f26dd5
apo <fukick@gmail.com>
2022-05-17 00:15:36 +0900

    ELMSから取得するものが仕様に沿っていなかったので書き換え

--- scraping.py
+++ scraping.py
@@ -1,39 +1,46 @@
-'''
-・ELMSのスクレイピングに関する処理を行う．
-・以下メソッド
-    ・getTimeList() -> タイムスタンプ一覧を返す
-    ・getTitleList() -> タイトル一覧を返す
-    ・login() -> ELMSログイン処理を行う
-    ・page_wait() -> ページのロードを待つ
-'''
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import datetime
 from selenium.webdriver.common.keys import Keys
 
-
 class ScrapeElms:
-    '''ELMSのスクレイピングに関わる処理をするクラス
-        ・ログインやMoodleのお知らせ情報をスクレイプする
-        ・https://www.elms.hokudai.ac.jp/portal/home/information/list より．
+    '''ELMSのURLでスクレイピングに関する処理を行うクラス
+    ・https://www.elms.hokudai.ac.jp/portal/home/information/list
 
-    Params:
-        ID : user id (string)
-        PASSWORD : user password (string)
-        time_stamp_list : タイムスタンプリスト(list[datetime])
-        title_list : タイトルリスト (list[string])
-        driver : Chrome driver(webdriver)，スクレイピング操作に用いるドライバ
-    Args:
-        ID(string)
-        PASSWORD(string)
+    Attributes
+    ---------
+        ID : string
+            ユーザーID 
+        PASSWORD : string
+            パスワード
+        select_index : int
+            個人・グループ・個人宛以外のどれを選択するかを保持する変数
+        time_stamp_list : list[`datetime`]
+            タイムスタンプリスト
+        title_list : list[`string`]
+            タイトルリスト
+        driver : 
+            Chrome driver，スクレイピング操作に用いるドライバ
     '''
+
     def __init__(self, ID, PASSWORD):
+        '''
+        Parameters
+        ----------
+        ID : string
+            ユーザーID
+        PASSWORD : string
+            パスワード
+        '''
+        
         # initialization
         self.ID = ID
         self.PASSWORD = PASSWORD
+        self.select_index = 1
         self.time_stamp_list = []
         self.title_list = []
 
@@ -42,15 +49,16 @@ class ScrapeElms:
         options.add_argument("--headless")
         self.driver = webdriver.Chrome(ChromeDriverManager().install(),
                                        options=options)
-        # self.driver = webdriver.Chrome(ChromeDriverManager().install(), ) # start driver with gui mode
+        # self.driver = webdriver.Chrome(ChromeDriverManager().install()) # start driver with gui mode
 
     def page_wait(self, class_name):
         '''ページのロードを待つ関数
-        driver.get()の後呼び出す．
-        引数にクラス要素名を指定する必要あり．
-
-        Args:
-            class_name (string)
+        driver.get()の後呼び出すのがよい．
+        
+        Args
+        ----
+        class_name : string
+            クラスの要素名を指定する．
         '''
         try:
             WebDriverWait(self.driver, 15).until(
@@ -67,36 +75,45 @@ class ScrapeElms:
             "https://www.elms.hokudai.ac.jp/portal/home/information/list")
         self.page_wait("signin-button")  # wait for page loading
 
-        id_input = self.driver.find_element(By.XPATH,
-                                            "//input[@id='username_input']")
+        id_input = self.driver.find_element_by_xpath("//input[@id='username_input']")
         id_input.send_keys(self.ID)
-        password_input = self.driver.find_element(
-            By.XPATH, "//input[@id='password_input']")
+        password_input = self.driver.find_element_by_xpath("//input[@id='password_input']")
         password_input.send_keys(self.PASSWORD)
 
-        login_button = self.driver.find_element(By.XPATH,
-                                                "//input[@id='login_button']")
-        self.page_wait("signin-button")
+        login_button = self.driver.find_element_by_xpath("//input[@id='login_button']")
         login_button.send_keys(Keys.ENTER)  # push Enter
         # login_button.click() #なぜかチャットBOTの画像が押されてしまうのでEnterに変更
 
-    def getTimeList(self):
+
+    def choose_dropdown_list(self, index):
+        '''ドロップダウンリスト(個人・グループ・個人宛以外)から一つを選択する
+        Args
+        ----
+        index : int
+            ドロップダウンリスト上から何番目の要素を取得するか
+            1:個人 2:グループ 3:個人宛以外
+        '''
+        self.select_index = index
+        self.page_wait("paging-txt")
+        dropdown = self.driver.find_element_by_xpath("//*[@id='informationType']")
+        select = Select(dropdown)
+        select.select_by_value(str(index))
+
+    def get_time_list(self):
         '''お知らせ一覧の時間を返す関数
 
-        Returns:
-            time_stamp_list : 時間リスト(降順)(list[datetime])
+        Returns
+        -------
+        time_stamp_list : list[`datetime`]
+            時間リスト(降順)
         '''
-        self.page_wait("result_paging_btn")  # wait for page loading
-        list_length = len(
-            self.driver.find_elements(
-                By.CLASS_NAME,
-                "result-list"))  # get how many elements there are
+        self.page_wait("paging-txt")  # wait for page loading
+        list_length = len(self.driver.find_elements_by_class_name("result-list"))  # get how many elements there are
 
         for i in range(1, list_length + 1):
             xpath = "//*[@id='information']/div/div[2]/div[{}]/div[2]/span[1]".format(
                 i)
-            time_stamp = self.driver.find_element(By.XPATH,
-                                                  xpath)  # find xpath
+            time_stamp = self.driver.find_element_by_xpath(xpath)  # find xpath
             inner_text = time_stamp.text  # get text in tag
             inner_text_dt = datetime.datetime.strptime(
                 inner_text, "%Y/%m/%d %H:%M")  # string to datetime
@@ -105,33 +122,42 @@ class ScrapeElms:
 
         return self.time_stamp_list
 
-    def getTitleList(self):
+    def get_title_list(self):
         '''お知らせ一覧のタイトルを返す関数
 
-        Returns:
-            title_list : タイトルリスト(降順)(list[string])
+        Returns
+        -------
+        title_list : list[`string`]
+            タイトルリスト(降順)
         '''
-        self.page_wait("result_paging_btn")  # wait for page loading
+        self.page_wait("paging-txt")  # wait for page loading
         list_length = len(
-            self.driver.find_elements(
-                By.CLASS_NAME,
-                "result-list"))  # get how many elements there are
+            self.driver.find_elements_by_class_name("result-list"))  # get how many elements there are
 
-        for i in range(1, list_length + 1):
-            xpath = "//*[@id='information']/div/div[2]/div[{}]/div[1]/span[2]".format(
-                i)
-            title = self.driver.find_element(By.XPATH, xpath)  # find xpath
+        for i in range(1, list_length + 1): #選択したものによってxpathの構造が変わっているので条件分岐
+            if self.select_index == 2: # グループ
+                xpath = "//*[@id='information']/div/div[2]/div[{}]/div[1]/span".format(
+                    i)
+            else: # 個人・個人宛以外
+                xpath = "//*[@id='information']/div/div[2]/div[{}]/div[1]/span[2]".format(
+                    i)
+
+            title = self.driver.find_element_by_xpath(xpath)  # find xpath
             inner_text = title.text  # get text in tag
             # print(inner_text)
             self.title_list.append(inner_text)  # add list
 
         return self.title_list
 
+    def close_browser(self):
+        self.driver.quit()
 
 if __name__ == "__main__":
     elms = ScrapeElms("{ユーザーID}", "{パスワード}")
     elms.login()
-    times_list = elms.getTimeList()
+    elms.choose_dropdown_list(1) #グループに関するお知らせ
+    times_list = elms.get_time_list()
     print("\n############ 時間一覧 ############\n times_list", times_list)
-    t_list = elms.getTitleList()
+    t_list = elms.get_title_list()
     print("\n############ タイトル一覧 ############\n title_list", t_list)
+    elms.close_browser()
