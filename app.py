c745a7c2157ad10608869fb58221dcdbc94fe9ad
Hiy4joMaho <lindemannmaho@gmail.com>
2022-05-21 14:14:02 +0900

    一時間おきにelms_info_pushのmain関数を起動

--- app.py
+++ app.py
@@ -1,49 +1,5 @@
-from flask import Flask, request, abort
+from elms_info_push import main
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import (
-    MessageEvent,
-    TextMessage,
-    TextSendMessage,
-)
-
-import settings
-
-app = Flask(__name__)
-
-
-line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
-
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print(
-            "Invalid signature. Please check your channel access token/channel secret."
-        )
-        abort(400)
-
-    return "OK"
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=event.message.text)
-    )
-
-
-if __name__ == "__main__":
-    app.run()
+scheduler = BlockingScheduler()
+scheduler.add_job(main, 'interval', hours=1)
