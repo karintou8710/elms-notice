fd4a9e134d04e7311634f7187bf19534d385f803
Hiy4joMaho <lindemannmaho@gmail.com>
2022-05-21 16:13:19 +0900

    試運転

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from elms_info_push import main
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
-scheduler.add_job(main, 'interval', hours=1)
+scheduler.add_job(main, 'interval', seconds=10)
 scheduler.start()
